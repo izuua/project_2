1f1b354f394f90673f620d79821a0a26a0c0346c
Robert Zuniga <github@mastertheday.com>
2019-08-17 10:17:29 -0400

    added api-doctor code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,53 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+
+  // Get route for getting all of the doctors
+  app.get("/api/doctors", function(req, res) {
+    db.Doctor.findAll({}).then(function(dbDoctor) {
+      res.json(dbDoctor);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+
+
+  // POST route for saving a new doctor
+  app.post("/api/doctors", function(req, res) {
+    db.Doctor.create(req.body).then(function(dbDoctor) {
+      res.json(dbDoctor);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+
+  // DELETE route for deleting doctors. 
+  //We can get the id of the doctor to be deleted from
+  // req.params.id
+  
+  app.delete("/api/doctors/:id", function(req, res) {
+    db.Doctor.destroy({ 
+      where: { 
+        id: req.params.id 
+      } 
+    }).then(function(dbDoctor) {
+      res.json(dbDoctor);
     });
   });
+
+
+ // PUT route for updating doctor. We can get the updated doctor data from req.body
+  app.put("/api/doctors", function(req, res) {
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    db.Doctor.update({
+      text: req.body.text,
+      complete: req.body.complete
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbDoctor) {
+      res.json(dbDoctor);
+    });
+  });
+
 };
