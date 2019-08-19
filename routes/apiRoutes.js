281aec3b19720a5d6fb8c7b6fa05c766abebe2d9
Robert Zuniga <robert@mastertheday.com>
2019-08-19 12:07:22 -0400

    fixed the key:values in api db references

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,60 +1,62 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   //////////////////////////////////
   //      dbDoctor api routes
   //////////////////////////////////
 
   // Get route for getting all of the doctors
-  app.get("/api/doctors", function (req, res) {
-    db.Doctor.findAll({}).then(function (dbDoctor) {
+  app.get("/api/doctors", function(req, res) {
+    db.Doctor.findAll({}).then(function(dbDoctor) {
       res.json(dbDoctor);
     });
   });
-
 
   // POST route for saving a new doctor
-  app.post("/api/doctors", function (req, res) {
+  app.post("/api/doctors", function(req, res) {
     db.Doctor.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
+      drFirstName: req.body.drFirstName,
+      drLastName: req.body.drLastName,
       contact: req.body.contact,
       specialty: req.body.specialty,
-      status: req.body.status 
-    }).then(function (dbDoctor) {
+      status: req.body.status
+    }).then(function(dbDoctor) {
       res.json(dbDoctor);
     });
   });
 
-  // DELETE route for deleting doctors. 
+  // DELETE route for deleting doctors.
   //We can get the id of the doctor to be deleted from
   // req.params.id
 
-  app.delete("/api/doctors/:id", function (req, res) {
+  app.delete("/api/doctors/:id", function(req, res) {
     db.Doctor.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function (dbDoctor) {
+    }).then(function(dbDoctor) {
       res.json(dbDoctor);
     });
   });
 
   // PUT route for updating doctor. We can get the updated doctor data from req.body
-  app.put("/api/doctors", function (req, res) {
+  app.put("/api/doctors", function(req, res) {
     // Update takes in an object describing the properties we want to update, and
     // we use where to describe which objects we want to update
-    db.Doctor.update({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      contact: req.body.contact,
-      specialty: req.body.specialty,
-      status: req.body.status
-    }, {
-      where: {
-        id: req.body.id
+    db.Doctor.update(
+      {
+        drFirstName: req.body.drFirstName,
+        drLastName: req.body.drLastName,
+        contact: req.body.contact,
+        specialty: req.body.specialty,
+        status: req.body.status
+      },
+      {
+        where: {
+          id: req.body.id
+        }
       }
-    }).then(function (dbDoctor) {
+    ).then(function(dbDoctor) {
       res.json(dbDoctor);
     });
   });
@@ -64,64 +66,63 @@ module.exports = function (app) {
   //////////////////////////////////
 
   // Get route for getting all of the patients
-  app.get("/api/patients", function (req, res) {
-    db.Patient.findAll({}).then(function (dbPatient) {
+  app.get("/api/patients", function(req, res) {
+    db.Patient.findAll({}).then(function(dbPatient) {
       res.json(dbPatient);
     });
   });
 
-
   // POST route for saving a new patient
-  app.post("/api/patients", function (req, res) {
+  app.post("/api/patients", function(req, res) {
     db.Patient.create({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
-      phoneNumber: req.body.phoneNumber,
-      email: req.body.email,
-      addressOne: req.body.addressOne,
-      addressTwo: req.body.addressTwo,
+      contact: req.body.contact,
+      address1: req.body.address1,
+      address2: req.body.address2,
       city: req.body.city,
       state: req.body.state,
       zipcode: req.body.zipcode
-    }).then(function (dbPatient) {
+    }).then(function(dbPatient) {
       res.json(dbPatient);
     });
   });
 
-  // DELETE route for deleting patients. 
+  // DELETE route for deleting patients.
   //We can get the id of the patient to be deleted from
   // req.params.id
 
-  app.delete("/api/patients/:id", function (req, res) {
+  app.delete("/api/patients/:id", function(req, res) {
     db.Patient.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function (dbPatient) {
+    }).then(function(dbPatient) {
       res.json(dbPatient);
     });
   });
 
   // PUT route for updating patient. We can get the updated patient data from req.body
-  app.put("/api/doctors", function (req, res) {
+  app.put("/api/doctors", function(req, res) {
     // Update takes in an object describing the properties we want to update, and
     // we use where to describe which objects we want to update
-    db.Doctor.update({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      phoneNumber: req.body.phoneNumber,
-      email: req.body.email,
-      addressOne: req.body.addressOne,
-      addressTwo: req.body.addressTwo,
-      city: req.body.city,
-      state: req.body.state,
-      zipcode: req.body.zipcode
-
-    }, {
-      where: {
-        id: req.body.id
+    db.Doctor.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        contact: req.body.contact,
+        address1: req.body.address1,
+        address2: req.body.address2,
+        city: req.body.city,
+        state: req.body.state,
+        zipcode: req.body.zipcode
+      },
+      {
+        where: {
+          id: req.body.id
+        }
       }
-    }).then(function (dbPatient) {
+    ).then(function(dbPatient) {
       res.json(dbPatient);
     });
   });
@@ -131,54 +132,50 @@ module.exports = function (app) {
   //////////////////////////////////
 
   // Get route for getting all of the visits
-  app.get("/api/visits", function (req, res) {
-    db.Visit.findAll({}).then(function (dbVisit) {
+  app.get("/api/visits", function(req, res) {
+    db.Visit.findAll({}).then(function(dbVisit) {
       res.json(dbVisit);
     });
   });
 
   // POST route for saving a new visit
-  app.post("/api/visits", function (req, res) {
+  app.post("/api/visits", function(req, res) {
     db.Visit.create({
-      firstName: req.body.firstName,
-      LastName: req.body.LastName,
-      notes: req.body.notes,
-      date: req.body.date
-    }).then(function (dbVisit) {
+      notes: req.body.notes
+    }).then(function(dbVisit) {
       res.json(dbVisit);
     });
   });
 
-  // DELETE route for deleting visits. 
+  // DELETE route for deleting visits.
   //We can get the id of the visit to be deleted from
   // req.params.id
 
-  app.delete("/api/visits/:id", function (req, res) {
+  app.delete("/api/visits/:id", function(req, res) {
     db.Visit.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function (dbVisit) {
+    }).then(function(dbVisit) {
       res.json(dbVisit);
     });
   });
 
-  // PUT route for updating doctor. We can get the updated visit data from req.body
-  app.put("/api/visits", function (req, res) {
+  // PUT route for updating visits . We can get the updated visit data from req.body
+  app.put("/api/visits", function(req, res) {
     // Update takes in an object describing the properties we want to update, and
     // we use where to describe which objects we want to update
-    db.Visit.update({
-      firstName: req.body.firstName,
-      LastName: req.body.LastName,
-      notes: req.body.notes,
-      date: req.body.date
-    }, {
-      where: {
-        id: req.body.id
+    db.Visit.update(
+      {
+        notes: req.body.notes
+      },
+      {
+        where: {
+          id: req.body.id
+        }
       }
-    }).then(function (dbVisit) {
+    ).then(function(dbVisit) {
       res.json(dbVisit);
     });
   });
-
 };
