fbe1544577149f6544227caa20c2c5edffc80aa5
dps0610 <dps0610@hotmail.com>
2019-08-19 14:06:08 -0400

    html routes working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,12 +6,21 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  // patient route
+  app.get("/patients", function(req, res) {
     // eslint-disable-next-line prettier/prettier
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+    db.Patient.findAll({}).then(function(patients) {
+      res.render("patient", {
+        patients: patients
+      });
+    });
+  });
+  //doctor route
+  app.get("/doctor/:id", function(req, res) {
+    // eslint-disable-next-line prettier/prettier
+    db.Patient.findOne({ where: { id: req.params.id }}).then(function(doctor) {
+      res.render("doctor", {
+        doctors: doctor
       });
     });
   });
