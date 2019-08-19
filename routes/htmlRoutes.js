0732628b17c0c70e45e20e8ae1ded0be4cd757dd
Mayenga <bndicunguye@gmail.com>
2019-08-19 12:20:24 -0400

    adding html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require('path')
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname,"../views/index1.html"))
+    res.sendFile(path.join(__dirname,"../views/index.html"))
   });
 
 
