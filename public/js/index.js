52205e5ef351e596d791c39e27773196ba102ea8
izuua <izuuaaf@gmail.com>
2019-08-20 11:11:29 -0400

    Added logic to get data from the new patient form

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -16,13 +16,13 @@ var API = {
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
@@ -31,9 +31,9 @@ var API = {
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
       var $a = $("<a>")
         .text(example.text)
         .attr("href", "/example/" + example.id);
@@ -61,7 +61,7 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var example = {
@@ -74,7 +74,7 @@ var handleFormSubmit = function(event) {
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveExample(example).then(function () {
     refreshExamples();
   });
 
@@ -84,12 +84,12 @@ var handleFormSubmit = function(event) {
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshExamples();
   });
 };
@@ -98,9 +98,38 @@ var handleDeleteBtnClick = function() {
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
 
-$("#signInButton").on("click", function() {
-  var doctorId = $("#doctorId").val().trim()
+$("#sign-in-btn").on("click", function (event)) {
+  event.preventDefault();
+
+  var doctorId = $("#doctor-id").val().trim()
   var url = "/doctor/" + doctorId
   console.log(url);
   window.location = url;
+});
+
+$("#new-patient-btn").on("click", function (event) {
+  event.preventDefault();
+
+  var newPatient = {
+    firstName: $("#new-first-name").val().trim(),
+    lastName: $("#new-last-name").val().trim(),
+    phone: $("#new-phone").val().trim(),
+    email: $("#new-email").val().trim(),
+    address: $("#new-address").val().trim(),
+    city: $("#new-city").val().trim(),
+    state: $("#new-state").val().trim(),
+    zip: $("#new-zip").val().trim()
+  };
+
+  console.log(newPatient);
+
+  // $.ajax({
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   type: "POST",
+  //   url: "api/doctors/",
+  //   data: JSON.stringify(newPatient)
+  // })
+
 })
