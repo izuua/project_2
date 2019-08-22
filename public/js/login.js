632fe3a9bb5bda873e501404f9b4c679f787f8e6
Robert Zuniga <robert@mastertheday.com>
2019-08-22 12:10:55 -0400

    update

--- public/js/login.js
+++ public/js/login.js
@@ -15,21 +15,21 @@ $(document).ready(function () {
     };
     console.log("Login userData ==> ", userData);
 
+    if (!userData.email || !userData.password) {
+      return;
+    
+    }
     // eslint-disable-next-line prettier/prettier
     var doctorId = $("#doctor-id").val().trim();
     var url = "/doctor?id=" + doctorId;
     // var url = "/doctor?id=";
-
     console.log("sign-in-btn url ==> ", url);
-     window.location = url;
-    // if (!userData.email || !userData.password) {
-    //   return;
-    // }
+    // window.location = url;
 
-    // // If we have an email and password we run the loginUser function and clear the form
-    // loginUser(userData.email, userData.password);
-    // emailInput.val("");
-    // passwordInput.val("");
+    // If we have an email and password we run the loginUser function and clear the form
+   // loginUser(userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
   });
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
@@ -39,7 +39,14 @@ $(document).ready(function () {
         password: password
       })
       .then(function () {
-        window.location.replace("/doctor?id=" + doctorId);
+        // eslint-disable-next-line prettier/prettier
+        var doctorId = $("#doctor-id").val().trim();
+        var url = "/doctor?id=" + doctorId;
+        // var url = "/doctor?id=";
+        console.log("sign-in-btn url ==> ", url);
+        window.location = url;
+
+        // window.location.replace("/doctor?id=" + doctorId);
         // If there's an error, log the error
       })
       .catch(function (err) {
